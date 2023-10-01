1bcfeaf14c454f1dbfc6a4d5b8475f41dca6c953
rajeevkumar2310 <rajeevkumar.gorikapudi@gmail.com>
2023-10-01 17:24:41 +0530

    episode 1

--- App.js
+++ App.js
@@ -4,13 +4,16 @@
         
        //  const heading = React.createElement("h1",{id:"heading",xyz:"abc"},"Hello World from React App.js File!");
         
+       import React from "react"
+       import ReactDOM from "react-dom"
+
        const parent = React.createElement("div",{id:"parent"},
        [React.createElement("div",{id:"child1"},[
-              React.createElement("h1",{},"this is h1"),
+              React.createElement("h1",{},"this is kpv of India"),
               React.createElement("h2",{},"this is h2"),
        ]),React.createElement("div",{id:"child2"},[
               React.createElement("h1",{},"this is h1"),
-              React.createElement("h2",{},"this is h2"),
+              React.createElement("h2",{},"By Rajeev Kumar"),
        ])]
        )
 
--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # Namaste React
+
+# Parcel
+Dev Build
+Local server
+HMR: Hot Module Replacement
+File watching algorithm -- written in C++
+Caching - Faster Builds
+Image optimization
+Minification
+Bundling
+Compression
+Consistent Hashing
+Code Splitting
+Differential Bundling - to support older browsers
+Diagnostics
+Good Error Hangling
+Https
+Tree Shaking - removes unused code
+Different build for Dev and Production
